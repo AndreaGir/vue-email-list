226331051221b59561e76b5edf187debc4420498
AndreaGir <girolamiandrea2000@gmail.com>
2022-03-07 18:27:03 +0100

    ...

---
+++ app.js
@@ -0,0 +1,25 @@
+const app = new Vue (
+
+    {
+        el: '#app',
+
+        data: {
+            email : [],
+        },
+
+        methods: {
+
+        },
+
+        mounted(){
+            for(let i=0; i <10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.email.push(response.data.response);
+                });      
+            }
+        }
+
+    }
+
+);
